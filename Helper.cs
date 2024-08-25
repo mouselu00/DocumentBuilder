34ceb656fe951828b78de6cfebb28ae46197a619
hayward lu <haywardlu00@gmail.com>
2024-08-25 20:12:52 +0800

    添加项目文件。

---
+++ Helper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentBuilder
+{
+    public class Helper
+    {
+        /// <summary>
+        /// 取得日期：yyyyMMDD
+        /// </summary>
+        /// <returns></returns>
+        public string getDataYYYYMMDD()
+        {
+            return DateTime.Now.ToString("yyyyMMdd");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+//https://dotnet.github.io/docfx/docs/dotnet-api-docs.html
+// https://www.cnblogs.com/memoyu/p/16896265.html
